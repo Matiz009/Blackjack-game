09019f3dbc38e8caf753d93ba2ad6be16902dae4
MATI UL REHMAN <iammatiz00@gmail.com>
2022-08-13 01:04:10 +0500

    a basic setup

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
-let firstCard = 10;
-let secondCard = 11;
-let sum = firstCard + secondCard + 4;
+let firstCard = Math.floor(Math.random() * 10) + 1;
+let secondCard = Math.floor(Math.random() * 10) + 1;
+let newCard = Math.floor(Math.random() * 10) + 1;
+let cards = [firstCard, secondCard];
+let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = " ";
-// 1. Declare a variable called message and assign its value to an empty string
 
-// 2. Reassign the message variable to the string we're logging out
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂";
-    console.log(message);
-} else if (sum === 21) {
-    message = "Woo hoo! You've got Blackjack! 🥳";
-    console.log(message);
-    hasBlackJack = true;
-} else {
-    message = "You're out of the game! 😭";
-    console.log(message);
-    isAlive = false;
+let btn = document.getElementById("btn");
+
+let btn_2 = document.getElementById("btn-2");
+
+btn.addEventListener("click", buttonClicked);
+btn_2.addEventListener("click", function() {
+    sum += newCard;
+    buttonClicked();
+    console.log("Draw a new card!");
+    alert("Draw a new card!");
+});
+
+message_el = document.getElementById("message-el");
+
+sum_el = document.getElementById("sum-el");
+
+cards_el = document.getElementById("cards-el");
+
+function buttonClicked() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂";
+        console.log(message);
+        message_el.textContent = message;
+        sum_el.textContent = "Sum: " + sum;
+        cards_el.textContent = "Cards: " + cards[0] + " & " + cards[1];
+    } else if (sum === 21) {
+        message = "Woo hoo! You've got Blackjack! 🥳";
+        console.log(message);
+        hasBlackJack = true;
+        message_el.textContent = message;
+        sum_el.textContent = "Sum: " + sum;
+        cards_el.textContent = "Cards: " + cards[0] + " & " + cards[1];
+    } else {
+        message = "You're out of the game! 😭";
+        console.log(message);
+        isAlive = false;
+        message_el.textContent = message;
+        sum_el.textContent = "Sum: " + sum;
+        cards_el.textContent = "Cards: " + cards[0] + " & " + cards[1];
+    }
 }
-
-// 3. Log it out!
